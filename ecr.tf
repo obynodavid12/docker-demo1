d81f6bbe03673c2c9a93d67eaeff070fc5d7bc1f
obynodavid12 <obynodavid12@gmail.com>
2020-11-03 22:59:10 +0000

    ecr-image

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,3 @@
 resource "aws_ecr_repository" "myapp" {
-  name = "ecr-app"
+  name = "myapp"
 }
--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
-output "myecr-repository-URL" {
-  value = aws_ecr_repository.ecr-app.repository_url
+output "myapp-repository-URL" {
+  value = aws_ecr_repository.myapp.repository_url
 }
 
 
